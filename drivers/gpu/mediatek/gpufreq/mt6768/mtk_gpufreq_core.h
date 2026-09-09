d49ae4de695d04886e7baedb0d28a23551c99227
Jeannie <fareldwianggara05@gmail.com>
2026-09-09 17:53:19 +0000

    Revert "Update GPU DVFS frequency for SEG_GPU_DVFS_FREQ0"
    This reverts commit 3d5b0083ea0d7a93280ce21993a1290053190a8d.

--- drivers/gpu/mediatek/gpufreq/mt6768/mtk_gpufreq_core.h
+++ drivers/gpu/mediatek/gpufreq/mt6768/mtk_gpufreq_core.h
@@ -17,7 +17,7 @@
 /**************************************************
  * MT6768/MT6767 : GPU DVFS OPP table Setting
  **************************************************/
-#define SEG_GPU_DVFS_FREQ0			(1450000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ0			(1310000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ1			(1150000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ2			(989000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ3			(970000)	/* KHz */
